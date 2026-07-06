99fbc6ad28fd08666721ae41cf6c6fec3471b108
Maurilio Bueno <mbueno@bueno-it.com>
2026-07-06 09:11:48 +0100

    fix: use python3 instead of python in cron entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@
 apt-get update -qq && apt-get install -y cron -qq
 
 # Criar crontab
-echo "0 7 * * * python /app/scheduled_list_of_employees_linux.py >> /var/log/cron.log 2>&1" > /etc/cron.d/list-of-employees
-echo "0 10 * * * python /app/scheduled_list_of_employees_linux.py >> /var/log/cron.log 2>&1" >> /etc/cron.d/list-of-employees
-echo "30 15 * * * python /app/scheduled_list_of_employees_linux.py >> /var/log/cron.log 2>&1" >> /etc/cron.d/list-of-employees
-echo "30 23 * * * python /app/scheduled_list_of_employees_linux.py >> /var/log/cron.log 2>&1" >> /etc/cron.d/list-of-employees
+echo "0 7 * * * python3 /app/scheduled_list_of_employees_linux.py >> /var/log/cron.log 2>&1" > /etc/cron.d/list-of-employees
+echo "0 10 * * * python3 /app/scheduled_list_of_employees_linux.py >> /var/log/cron.log 2>&1" >> /etc/cron.d/list-of-employees
+echo "30 15 * * * python3 /app/scheduled_list_of_employees_linux.py >> /var/log/cron.log 2>&1" >> /etc/cron.d/list-of-employees
+echo "30 23 * * * python3 /app/scheduled_list_of_employees_linux.py >> /var/log/cron.log 2>&1" >> /etc/cron.d/list-of-employees
 
 chmod 0644 /etc/cron.d/list-of-employees
 crontab /etc/cron.d/list-of-employees
